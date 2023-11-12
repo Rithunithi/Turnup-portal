de00a2b2080b103b6357a8113cd88ba4f030a37b
Manju Ramesh <manjukamaraj84@gmail.com>
2023-11-13 12:38:07 +1300

    adding initial code for Turn up portal

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+driver.Manage().Window.Maximize();
+
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login");
+
+IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+usernameTextbox.SendKeys("hari");
+
+IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+passwordTextbox.SendKeys("123123");
+
+IWebElement loginbutton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+loginbutton.Click();
+
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("User has logged in successfully");
+}
+else
+{
+    Console.WriteLine("User hasn't been logged in");
+}
